5361217822c249485d64e27c77e287db5a2f2e0f
gabriel uzoh-amobi <uzoamobi@MacBook-Pro.fritz.box>
2021-12-15 18:40:30 +0000

    removed rates currency one in js

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function calculate() {
     .then(res => res.json())
     .then(data => {
       //  console.log(data);
-      const rate = data.rates[currency_two] / data.rates[currency_one];
+      const rate = data.rates[currency_two];
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-      amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
     });
 }
 
